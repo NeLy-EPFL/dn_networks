fb4d2db558695c255c2ffdfa87f6948a6d54f4bb
fhurtak <femke.hurtak@gmail.com>
2024-01-30 17:39:23 +0100

    generating automatically additional connectomics plots

--- dn_connectomics/make_all_plots.py
+++ dn_connectomics/make_all_plots.py
@@ -6,11 +6,11 @@ Script to make all the plots for the paper.
 import os
 
 from flower_plots import draw_all_flower_plots
-from connectivity_stats import compute_connectivity_stats
+from connectivity_stats import compute_connectivity_stats, compare_connectivity_stats
 from distribution_fit import compare_distributions
 from louvain_clustering import draw_louvain_custering
 from louvain_communities_control import compare_clustering_with_random
-from draw_meta_network import draw_meta_network
+from draw_meta_network import draw_meta_network, draw_network_organised_by_clusters
 from zoom_subsets_network import draw_subsets_network
 from detail_nt_matrix import detail_neurotransmitters_matrix
 from connectivity_vs_recurrence import connectivity_vs_recurrence_plots
@@ -18,6 +18,7 @@ from layer_reaching_plot import cumulative_diffusion_plot
 from brain_synapses_distribution import plot_brain_synapses_distribution
 from cluster_behavior_analysis import make_cluster_analysis_plots
 import params
+import plot_params
 
 
 if __name__ == "__main__":
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     os.makedirs(figures_path, exist_ok=True)
 
     # --- subnetwork visualisations --- #
-    print("=== Drawing flower plots, fig. 3 c-d & fig. 5 e")
+    print("=== Drawing flower plots, fig. 3 c-d, fig. 5 e, ED fig. 6, 7, 8 & 9")
     draw_all_flower_plots()
 
     # --- connectivity statistics --- #
@@ -35,6 +36,12 @@ if __name__ == "__main__":
     compare_distributions()
     print("=== Computing cumulative diffusion, fig. 3 e")
     cumulative_diffusion_plot()
+    print("== Comparing distributions with different underlying graph models, ED fig. 5")
+    compare_connectivity_stats(
+        direction = plot_params.INTERNEURON_STATS_ARGS["measured_feature"],
+        connection = plot_params.INTERNEURON_STATS_ARGS["connection_type"],
+        reference_sorting = plot_params.INTERNEURON_STATS_ARGS["reference_sorting"],
+    )
 
     # --- clustering visualisations --- #
     print("=== Drawing Louvain clustering, fig 6")
@@ -49,3 +56,10 @@ if __name__ == "__main__":
     plot_brain_synapses_distribution()
     print("=== Showing where clusters output to in the VNC, which behaviors & DNs are represented, fig 6 f,g,j")
     make_cluster_analysis_plots()
+    print("=== Detail of inter-cluster connectivity at neuron-level resolution, ED fig. 10")
+    draw_network_organised_by_clusters(
+        restricted_nodes = plot_params.NETWORK_PLOT_ARGS["restricted_nodes"],
+        restricted_clusters = plot_params.NETWORK_PLOT_ARGS["restricted_clusters"],
+        restricted_connections = plot_params.NETWORK_PLOT_ARGS["restricted_connections"],
+        position_reference = plot_params.NETWORK_PLOT_ARGS["position_reference"],
+    )
